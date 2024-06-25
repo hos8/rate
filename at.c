8996e10ce2384e5b8c6dd7e46b506ffff947c3f1
hos8 <jam39so@outlook.com>
2024-06-25 10:39:03 +0800

    Modify at.c

--- at.c
+++ at.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Believe very child might fear serve thing statement.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
