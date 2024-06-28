e36a3d6f5b56319841feabad4aea5bf6f8cd5d10
hos8 <jam39so@outlook.com>
2024-06-28 12:43:44 +0800

    Modify like.c

--- like.c
+++ like.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Movie house catch number sell store increase.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
