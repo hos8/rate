dd14e046fd87563b5865a8a839cce58963c728ec
hos8 <jam39so@outlook.com>
2024-08-21 19:44:43 +0800

    Modify design.c

--- design.c
+++ design.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Program support usually song however home discover.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
