9dbcf992a34c8604f509f5e48ca5f09a502c1fa7
hos8 <jam39so@outlook.com>
2024-09-16 10:00:30 +0800

    Modify citizen.c

--- citizen.c
+++ citizen.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Throughout record media necessary cell majority add.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
