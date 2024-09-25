5dd471a2e7ac7d7c81e00ca2380630d8505f1056
hos8 <jam39so@outlook.com>
2024-09-25 11:27:07 +0800

    Modify yes.cpp

--- yes.cpp
+++ yes.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Spring leg better look movie.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
