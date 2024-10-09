1abe822c93f30225f86a03dbcde04c54457aa574
hos8 <jam39so@outlook.com>
2024-10-09 10:49:23 +0800

    Modify later.cpp

--- later.cpp
+++ later.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Response feel forget.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
