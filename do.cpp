3975944d1bc3f4f553401e577bac3dfb42e328ce
hos8 <jam39so@outlook.com>
2024-10-10 13:01:27 +0800

    Modify do.cpp

--- do.cpp
+++ do.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Party concern military back.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
