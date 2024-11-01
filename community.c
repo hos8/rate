e14a350571dd1ef2a5a509f4ced7527a5a8f4d0c
hos8 <jam39so@outlook.com>
2024-11-01 12:52:42 +0800

    Modify community.c

---
+++ community.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
+
+# Occur fall check research interview.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
