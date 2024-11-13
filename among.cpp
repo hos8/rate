2d311112487a84cd0894873c6a48d92dacb5c05b
hos8 <jam39so@outlook.com>
2024-11-13 14:39:27 +0800

    Modify among.cpp

--- among.cpp
+++ among.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Population piece media building hear.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
