06846394776776bc753de105e2988c44f0ff8558
hos8 <jam39so@outlook.com>
2024-11-27 12:57:14 +0800

    Modify lawyer.cpp

--- lawyer.cpp
+++ lawyer.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Could us many.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
