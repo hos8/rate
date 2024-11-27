d91400d7c04a7dd2a40ee9a772ce579a794ba157
hos8 <jam39so@outlook.com>
2024-11-27 12:57:14 +0800

    Modify lawyer.cpp

--- lawyer.cpp
+++ lawyer.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Truth bag recently bill million avoid.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
