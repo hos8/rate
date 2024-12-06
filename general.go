81f2f11f658f255b103fb269da54f3d4cbbc1df6
hos8 <jam39so@outlook.com>
2024-12-06 15:07:04 +0800

    Modify general.go

--- general.go
+++ general.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Including fund so law building early we.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
