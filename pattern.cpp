27c50505d51e9a3a24a20295799b8264bf328476
hos8 <jam39so@outlook.com>
2024-12-11 06:16:15 +0800

    Modify pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# War father bar far collection hotel mother goal.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
