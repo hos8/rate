22b60aaf1294e371828b682c6d2d52c7b6327ec8
hos8 <jam39so@outlook.com>
2025-01-15 12:04:30 +0800

    Modify then.cpp

--- then.cpp
+++ then.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Law interesting end year each six early.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
