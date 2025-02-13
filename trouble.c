2536cf211e84c565f49442e5efec2a212c2a9947
hos8 <jam39so@outlook.com>
2025-02-13 14:53:48 +0800

    Modify trouble.c

--- trouble.c
+++ trouble.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Shoulder almost final reduce eat pick throw.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
