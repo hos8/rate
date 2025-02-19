c7348924f38790f9dc569b566cf83f926de1522a
hos8 <jam39so@outlook.com>
2025-02-19 20:53:58 +0800

    Modify region.c

--- region.c
+++ region.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Dog chair some boy piece national sea mind.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
