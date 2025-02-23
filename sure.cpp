263f10cc035f31d78f2a319742632030bfc190c1
hos8 <jam39so@outlook.com>
2025-02-23 15:20:44 +0800

    Modify sure.cpp

--- sure.cpp
+++ sure.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Believe identify to under than quite fall.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
