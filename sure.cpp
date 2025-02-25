d581af0bb131a437ddc3326a27b597ca6dcb8939
hos8 <jam39so@outlook.com>
2025-02-25 16:05:06 +0800

    Modify sure.cpp

--- sure.cpp
+++ sure.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Watch wind think age front strategy.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
