3cd769707d410e08a137df3274090074701fa8fc
hos8 <jam39so@outlook.com>
2025-02-25 16:05:06 +0800

    Modify sure.cpp

--- sure.cpp
+++ sure.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# In tree maybe serious.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
